3857cd9d70e8ef5c8dd546197da2be63905ccf1f
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-08-19 00:46:34 -0500

    first commit

---
+++ src/main/java/com/pragma/emason/EmasonApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.emason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmasonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/emason/EmasonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.emason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
