6d513d9cbe600490805da505bb1fb4da319a8398
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-08-26 15:48:05 -0500

    test: add test to CategoryHandler, CategoryUseCase and CategoryJpaAdapter classes

---
+++ src/test/java/com/pragma/emason/infrastructure/output/jpa/adapter/CategoryJpaAdapterTest.java
@@ -0,0 +1,74 @@
+package com.pragma.emason.infrastructure.output.jpa.adapter;
+
+import com.pragma.emason.infrastructure.output.jpa.repository.ICategoryRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.pragma.emason.domain.model.Category;
+import com.pragma.emason.domain.model.PageResult;
+import com.pragma.emason.infrastructure.output.jpa.entity.CategoryEntity;
+
+import com.pragma.emason.infrastructure.output.jpa.mapper.ICategoryEntityMapper;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.Arrays;
+
+import java.util.List;
+
+
+import static org.mockito.Mockito.*;
+
+class CategoryJpaAdapterTest {
+
+    @Mock
+    private ICategoryRepository iCategoryRepository;
+
+    @Mock
+    private ICategoryEntityMapper iCategoryEntityMapper;
+
+    @InjectMocks
+    private CategoryJpaAdapter categoryJpaAdapter;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getAllCategoriesSuccessfully() {
+        // Configurar datos de prueba
+        CategoryEntity categoryEntity1 = new CategoryEntity(1, "Category 1", "Description 1");
+        CategoryEntity categoryEntity2 = new CategoryEntity(2, "Category 2", "Description 2");
+        List<CategoryEntity> categoryEntities = Arrays.asList(categoryEntity1, categoryEntity2);
+        Page<CategoryEntity> categoryEntityPage = new PageImpl<>(categoryEntities, PageRequest.of(0, 2, Sort.by(Sort.Direction.ASC, "name")), 10);
+
+        // Configurar mock del repositorio
+        when(iCategoryRepository.findAll(any(PageRequest.class))).thenReturn(categoryEntityPage);
+
+        // Configurar mock del mapper
+        Category category1 = new Category(1, "Category 1", "Description 1");
+        Category category2 = new Category(2, "Category 2", "Description 2");
+        when(iCategoryEntityMapper.toCategory(categoryEntity1)).thenReturn(category1);
+        when(iCategoryEntityMapper.toCategory(categoryEntity2)).thenReturn(category2);
+
+        // Ejecutar el método a probar
+        PageResult<Category> result = categoryJpaAdapter.getAllCategories(0, 2, "name", true);
+
+        // Verificar el resultado
+        assertEquals(2, result.getContent().size());
+        assertEquals(0, result.getPage());
+        assertEquals(2, result.getSize());
+        assertEquals(10, result.getTotalElements());
+        assertEquals("Category 1", result.getContent().get(0).getName());
+        assertEquals("Category 2", result.getContent().get(1).getName());
+    }
+}
