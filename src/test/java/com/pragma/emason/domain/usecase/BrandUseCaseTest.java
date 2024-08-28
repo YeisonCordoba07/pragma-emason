d1c6ba7fd545fbac7364a45c49fd539a7fd426ab
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-08-27 19:24:40 -0500

    test: add test to BrandUseCase class

--- src/test/java/com/pragma/emason/domain/usecase/BrandUseCaseTest.java
+++ src/test/java/com/pragma/emason/domain/usecase/BrandUseCaseTest.java
@@ -1,5 +1,6 @@
 package com.pragma.emason.domain.usecase;
 
+import com.pragma.emason.domain.exception.BrandNameAlreadyExistsException;
 import com.pragma.emason.domain.model.Brand;
 import com.pragma.emason.domain.spi.IBrandPersistence;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,8 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class BrandUseCaseTest {
 
@@ -24,17 +25,75 @@ class BrandUseCaseTest {
         MockitoAnnotations.openMocks(this);
     }
 
+
+
+
     @Test
-    void testSaveBrand_ShouldSaveBrand() {
+    void saveBrand_BrandDoesNotExist_ShouldSaveBrand() {
         // Arrange
-        Brand brand = new Brand();
-        brand.setName("Brand's name");
-        brand.setDescription("Brand's description");
+        String brandName = "NewBrand";
+        Brand newBrand = new Brand(brandName, "New Brand Description");
+        when(iBrandPersistence.getBrandByName(brandName)).thenReturn(null);
 
         // Act
-        brandUseCase.saveBrand(brand);
+        brandUseCase.saveBrand(newBrand);
 
         // Assert
-        verify(iBrandPersistence, times(1)).saveBrand(brand);
+        verify(iBrandPersistence, times(1)).getBrandByName(brandName);
+        verify(iBrandPersistence, times(1)).saveBrand(newBrand);
+    }
+
+
+    @Test
+    void saveBrand_WhenBrandExist_ShouldReturnThrowsException() {
+        // Arrange
+        String brandName = "ExistingBrand";
+        Brand existingBrand = new Brand(brandName, "Existing Brand Description");
+        when(iBrandPersistence.getBrandByName(brandName)).thenReturn(existingBrand);
+
+        Brand newBrand = new Brand(brandName, "New Brand Description");
+
+        // Act & Assert
+        assertThrows(BrandNameAlreadyExistsException.class, () -> {
+            brandUseCase.saveBrand(newBrand);
+        });
+
+        // Verifications
+        verify(iBrandPersistence, times(1)).getBrandByName(brandName);
+        verify(iBrandPersistence, never()).saveBrand(any(Brand.class));
+    }
+
+
+
+
+    @Test
+    void getBrandByName_WhenBrandExist_ShouldGettingBrandSuccessfully(){
+        // Arrange
+        String brandName = "BrandTest";
+        Brand expectedBrand = new Brand(brandName, "Brand Description");
+        when(iBrandPersistence.getBrandByName(brandName)).thenReturn(expectedBrand);
+
+        // Act
+        Brand actualBrand = brandUseCase.getBrandByName(brandName);
+
+        // Assert
+        assertEquals(expectedBrand, actualBrand);
+        verify(iBrandPersistence, times(1)).getBrandByName(brandName);
+
+    }
+
+
+    @Test
+    void getBrandByName_WhenBrandDoesNotExist_ShouldReturnNull() {
+        // Arrange
+        String brandName = "NonExistentBrand";
+        when(iBrandPersistence.getBrandByName(brandName)).thenReturn(null);
+
+        // Act
+        Brand actualBrand = brandUseCase.getBrandByName(brandName);
+
+        // Assert
+        assertNull(actualBrand);
+        verify(iBrandPersistence, times(1)).getBrandByName(brandName);
     }
 }
